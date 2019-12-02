966811af105760d83c546815060d22097babcf6f
Dacio Romero <dacioromero@gmail.com>
2019-12-02 15:15:48 -0800

    Updates

---
+++ precache-manifest.cd5f4c11857321b890c8f18228ef6b19.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b63dd099c05fb9d346b5efe72232925",
+    "url": "/wm-events/index.html"
+  },
+  {
+    "revision": "779e4b14482288cd2b61",
+    "url": "/wm-events/static/js/2.46a8fc97.chunk.js"
+  },
+  {
+    "revision": "b31ae2effc7abc2e5019",
+    "url": "/wm-events/static/js/main.b4589761.chunk.js"
+  },
+  {
+    "revision": "5195c5aeb2552cad1c0a",
+    "url": "/wm-events/static/js/runtime-main.574dd7a6.js"
+  }
+]);
